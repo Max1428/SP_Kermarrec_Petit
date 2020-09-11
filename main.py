16eef3b2b457d77b8a2e9394196392dd0385d74f
Kathaya <ferdinandpetit92@gmail.com>
2020-09-11 10:11:05 +0200

    Main python file script

---
+++ main.py
@@ -0,0 +1,42 @@
+import argparse
+import numpy as np
+
+
+class proteine(object):
+	def __init__(self, file):
+		self.Calpha=np.array([0,0,0,0])
+		self.AA_name = []
+		with open(file, 'r') as fillin:
+			lines=fillin.readlines()
+			for line in lines:
+				#self.Calpha = np.vstack((self.Calpha, np.float_(line.split()[5:9]))) 
+				self.Calpha = np.vstack((self.Calpha, np.float_([line[22:26],line[30:38], line[38:46], line[46:54]]))) #get the number and position of all Calpha
+				self.AA_name.append(line[17:20])
+			self.Calpha = np.delete(self.Calpha, (0), axis = 0) #Delete the first row of the array fill with 0.
+			self.AA_name = np.array([self.AA_name])
+			
+			#self.Calpha = np.concatenate((self.Calpha, np.concatenate(self.AA_name)[:,None]), axis=1)
+			#self.Calpha = np.column_stack((self.Calpha, np.array([self.AA_name])))
+		self.center()
+
+	def center(self):
+		self.centre=list(np.mean(self.Calpha, axis=0))[1:4]
+
+	def __str__(self):
+		return("Gravity center : {}".format(self.centre))
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("PDBfile", help = "PDB file with only alpha carbons extracted, no header, can be done with calpha.py file", type=str)
+	args = parser.parse_args()
+
+
+
+	if args.PDBfile[-4:] != '.pdb': #Vérification de l'extension pdb
+		print("Please select file with pdb extension")
+	else: 
+		pro = proteine(args.PDBfile)
+		print(pro)
+
+
+
